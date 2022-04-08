e506ee76f83a1a6395855e3a89928bca3861b3bf
sadat.malik <sadat.malik@me.com>
2022-04-08 14:16:15 +0100

    basic blockchain classes

--- src/main/java/com/sadatmalik/blockchain/Block.java
+++ src/main/java/com/sadatmalik/blockchain/Block.java
@@ -1,5 +1,20 @@
-package com.sadatmalik.blockchain;/**
+package com.sadatmalik.blockchain;
+
+/**
  * @author sm@creativefusion.net
  */
 public class Block {
+    Long index;
+    String timeStamp;
+    String proof;
+    String previousHash;
+
+
+    Block(long index, String timeStamp, String proof,
+          String previousHash) {
+        this.index = index;
+        this.timeStamp = timeStamp;
+        this.proof = proof;
+        this.previousHash = previousHash;
+    }
 }
--- src/main/java/com/sadatmalik/blockchain/Blockchain.java
+++ src/main/java/com/sadatmalik/blockchain/Blockchain.java
@@ -1,7 +1,34 @@
 package com.sadatmalik.blockchain;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ * Creates a blockchain
+ *
  * @author sm@creativefusion.net
  */
 public class Blockchain {
+
+    List<Block> chain;
+
+    Blockchain() {
+        chain = new ArrayList<>();
+        Block genesis = createBlock("1", "0");
+    }
+
+    public Block createBlock(String proof, String previousHash) {
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String timeStamp = dateTime.format(formatter);
+
+        Block block = new Block(
+                chain.size()+1,
+                timeStamp, proof, previousHash);
+
+        chain.add(block);
+        return block;
+    }
 }
