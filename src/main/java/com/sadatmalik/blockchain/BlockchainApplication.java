fc1f5a7d624d5252ec1e634d160a2afdbeacf7e4
sadat.malik <sadat.malik@me.com>
2022-04-08 15:14:29 +0100

    setting up spring application rest application

---
+++ src/main/java/com/sadatmalik/blockchain/BlockchainApplication.java
@@ -0,0 +1,18 @@
+package com.sadatmalik.blockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Rest application.
+ *
+ * @author sadatmalik
+ */
+@SpringBootApplication
+public class BlockchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainApplication.class, args);
+	}
+
+}
