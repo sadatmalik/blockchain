d79104b3711dc08ffc6b6e2c49ce776b5f7cb7d3
sadat.malik <sadat.malik@me.com>
2022-04-11 12:18:46 +0100

    block doc

--- src/main/java/com/sadatmalik/blockchain/model/Block.java
+++ src/main/java/com/sadatmalik/blockchain/model/Block.java
@@ -3,7 +3,8 @@ package com.sadatmalik.blockchain.model;
 import lombok.Getter;
 
 /**
- * Models a block in the blockchain.
+ * Models a block in the blockchain. Encapsulate index, timeStamp, proof, and
+ * the previous block's SHA 256 ash.
  *
  * @author sm@creativefusion.net
  */
@@ -14,6 +15,14 @@ public class Block {
     long proof;
     String previousHash;
 
+    /**
+     * All args constructor.
+     *
+     * @param index block chain index
+     * @param timeStamp timestamp string
+     * @param proof the proof of work
+     * @param previousHash previous block's hash
+     */
     Block(long index, String timeStamp, long proof,
           String previousHash) {
         this.index = index;
