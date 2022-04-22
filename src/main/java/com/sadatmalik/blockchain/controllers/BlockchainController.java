d6557991a51b5bcc8275e8f7ab61f02c0c6558ef
sadat.malik <sadat.malik@me.com>
2022-04-22 14:10:17 +0100

    api v1. incremental #4

--- src/main/java/com/sadatmalik/blockchain/controllers/BlockchainController.java
+++ src/main/java/com/sadatmalik/blockchain/controllers/BlockchainController.java
@@ -9,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -25,6 +22,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/v1")
 public class BlockchainController {
 
     private final Blockchain blockchain;
